e1cd17f2132def5dae5d490b068d7daa34d3e82c
Hridya Agrawal <hridayahoney@gmail.com>
2022-11-22 00:23:57 +0530

    Add files via upload

---
+++ react-webcam/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import WebcamImage from "./image";
+import "./App.css";
+import WebcamVideo from "./video";
+
+function App() {
+  return (
+    <div className="App">
+      <WebcamImage />
+      <WebcamVideo />
+    </div>
+  );
+}
+
+export default App;
